e2ba83cd786402fda6de59b03b65f411d0491b42
hiyazakite <kaspar.holmgren@gmail.com>
2024-09-25 23:20:40 +0200

    added

--- src/utils/resetForms.ts
+++
@@ -1,5 +0,0 @@
-import { UseFormReturnType } from '@mantine/form';
-
-export function resetForms(forms: Array<UseFormReturnType<ExpenseValues> | UseFormReturnType<PersonValues>>) {
-    forms.forEach((form) => form.reset());
-};
